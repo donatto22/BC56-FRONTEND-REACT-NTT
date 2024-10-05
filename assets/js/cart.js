3be2c4e90ff11f10207dd28af08a7cc015442710
donatto22 <ottanod22@gmail.com>
2024-10-05 13:34:06 -0500

    feat: El carrito aumenta su valor por los click del producto
    Adicionalmente, mantiene su valor si se cierra la página o pasan días

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,9 +1,22 @@
 import { getItem, setItem } from './useLocalStorage.js'
 
+const cart = document.getElementById('cart')
+const counter = cart.querySelector('p')
+
 export const addToCart = () => {
     // is there already something added?
     const cartCounter = Number(getItem('cartCounter'))
     
     if(!cartCounter) setItem('cartCounter', 1)
         else setItem('cartCounter', cartCounter + 1)
+
+    counter.textContent = cartCounter + 1
+}
+
+export const verifyCounter = () => {
+    const cartCounter = getItem('cartCounter')
+    
+    if(cartCounter) {
+        counter.textContent = cartCounter
+    }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 import { getCategories, getProducts } from './api.js'
+import { verifyCounter } from './cart.js'
 import { renderProducts } from './functions.js'
 
 const { products } = await getProducts()
 
-renderProducts(products, 'products')
+// verify if there is already a cart counter
+verifyCounter()
 
+renderProducts(products, 'products')
