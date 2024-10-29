1ee511eb739678beea64d765e4ff39c411e3024d
donatto22 <ottanod22@gmail.com>
2024-10-29 11:38:08 -0500

    feat: Login terminado
    Se agrega la funcionalidad de contraseña olvidada

--- src/components/RecoverPassword/RecoverPasword.tsx
+++ src/components/RecoverPassword/RecoverPasword.tsx
@@ -1,14 +1,21 @@
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import './recoverPassword.css'
 
 import { useForm } from 'react-hook-form'
 import closeIcon from '@icons/close-outline.svg'
+import { toast } from 'sonner'
 
 const RecoverPasswordModal = ({ setModal }: { setModal: Dispatch<SetStateAction<boolean>> }) => {
-    const { register, handleSubmit } = useForm()
+    const [sendEmail, setSendEmail] = useState<boolean>(false)
+    const { register, handleSubmit, formState: { errors } } = useForm()
 
     const onSubmit = handleSubmit((data) => {
         console.log(data)
+
+        if(sendEmail === false) {
+            toast.success('Se envió un correo para la recuperación de contraseña a ' + data.correo)
+            setSendEmail(true)
+        } else toast.error('Ya se ha enviado un correo a ' + data.correo)
     })
 
     return (
@@ -21,11 +28,21 @@ const RecoverPasswordModal = ({ setModal }: { setModal: Dispatch<SetStateAction<
                 <h3>Recuperar contraseña</h3>
 
                 <form onSubmit={ onSubmit }>
-                    <input type='email' placeholder='Ingresa tu correo' { ...register('correo') } />
+                    <div> { errors.correo && errors.correo.message as string } </div>
+                    <input type='email' placeholder='Ingresa tu correo' { ...register('correo', {
+                        required: {
+                            value: true,
+                            message: 'Correo es requerido'
+                        },
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9]+\.[a-z]/,
+                            message: 'El correo no es válido'
+                        }
+                    }) } />
 
                     <div id="actionButtons">
                         <button type='submit'>Enviar</button>
-                        <button onClick={ () => setModal(prev => !prev) }>Cancelar</button>
+                        <button type='button' onClick={ () => setModal(prev => !prev) }>Cancelar</button>
                     </div>
                 </form>
             </div>
